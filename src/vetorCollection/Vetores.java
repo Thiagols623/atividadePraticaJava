7b2f479dc1971e789fec2e02f18ef089ef51e63e
Thiago Lima <thiagolima6623@gmail.com>
2026-04-24 19:41:22 -0300

    Atividade pratica de Vetor

---
+++ src/vetorCollection/Vetores.java
@@ -0,0 +1,33 @@
+package vetorCollection;
+
+import java.util.Scanner;
+
+public class Vetores {
+
+	public static void main(String[] args) {
+
+		Scanner leia = new Scanner(System.in);
+
+		int numeros[] = { 2, 5, 1, 3, 4, 9, 7, 8, 10, 6 };
+
+		System.out.print("Digite o número que você deseja encontrar: ");
+		int numeroBuscado = leia.nextInt();
+
+		int posicao = -1;
+
+		for (int i = 0; i < numeros.length; i++) {
+			if (numeros[i] == numeroBuscado) {
+				posicao = i;
+				break;
+			}
+		}
+
+		if (posicao >= 0) {
+			System.out.println("\nO número " + numeroBuscado + " está localizado na posição: " + posicao);
+		} else {
+			System.out.println("\nO número " + numeroBuscado + " não foi encontrado!");
+		}
+
+		leia.close();
+	}
+}
